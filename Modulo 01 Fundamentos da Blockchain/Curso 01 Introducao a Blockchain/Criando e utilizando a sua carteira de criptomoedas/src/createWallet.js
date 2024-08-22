c410407e7795cb2ba149a8ddbaebfa9f90cd934c
alinemach <alinemach@gmail.com>
2024-08-22 00:25:52 -0300

    readme criado

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
@@ -1,24 +1,22 @@
-//importando as dependencias
+
 const bip32 = require('bip32')
 const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
-//definir a rede
-//bitcoin - rede principal - mainnet
-//testnet - rede de teste - tesnet
+
 const network = bitcoin.networks.testnet
 
-//derivação de carteiras HD
+
 const path = `m/49'/1'/0'/0` 
 
-//criando o mnemonic para a seed (palavras de senha)
+
 let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
 
-//criando a raiz da cartiera HD
+
 let root = bip32.fromSeed(seed, network)
 
-//criando uma conta - par pvt-pub keys
+
 let account = root.derivePath(path)
 let node = account.derive(0).derive(0)
 
