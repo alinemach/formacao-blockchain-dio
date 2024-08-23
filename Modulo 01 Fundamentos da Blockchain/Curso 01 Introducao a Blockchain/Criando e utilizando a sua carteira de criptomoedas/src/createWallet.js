0dde00cb0e52f2d725ad827e83ba16634492cb4a
alinemach <alinemach@gmail.com>
2024-08-23 11:48:34 -0300

    path do testnet corrigido

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
@@ -1,31 +1,25 @@
+const bip32 = require('bip32');
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
 
-const bip32 = require('bip32')
-const bip39 = require('bip39')
-const bitcoin = require('bitcoinjs-lib')
+const network = bitcoin.networks.testnet;
 
+const path = `m/84'/1'/0'/0`;
 
-const network = bitcoin.networks.testnet
+let mnemonic = bip39.generateMnemonic();
+const seed = bip39.mnemonicToSeedSync(mnemonic);
 
+let root = bip32.fromSeed(seed, network);
 
-const path = `m/49'/1'/0'/0` 
+let account = root.derivePath(path);
+let node = account.derive(0).derive(0);
 
-
-let mnemonic = bip39.generateMnemonic()
-const seed = bip39.mnemonicToSeedSync(mnemonic)
-
-
-let root = bip32.fromSeed(seed, network)
-
-
-let account = root.derivePath(path)
-let node = account.derive(0).derive(0)
-
-let btcAddress = bitcoin.payments.p2pkh({
+let btcAddress = bitcoin.payments.p2wpkh({
     pubkey: node.publicKey,
     network: network,
-}).address
+}).address;
 
-console.log("Carteira gerada")
-console.log("Endereço: ", btcAddress)
-console.log("Chave privada:", node.toWIF())
-console.log("Seed:", mnemonic)
+console.log("Carteira gerada");
+console.log("Endereço: ", btcAddress);
+console.log("Chave privada:", node.toWIF());
+console.log("Seed:", mnemonic);
